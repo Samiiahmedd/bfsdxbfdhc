6a59dda089b0ba461b0b41cbe2c671f11f91a43f
Samiiahmed <eldon.smsm135@gmail.com>
2023-10-25 14:36:16 +0300

    Initial Commit

---
+++ bfsdxbfdhc/bfsdxbfdhcApp.swift
@@ -0,0 +1,17 @@
+//
+//  bfsdxbfdhcApp.swift
+//  bfsdxbfdhc
+//
+//  Created by Sami Ahmed on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct bfsdxbfdhcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
